ecf59270402faf25e7431c04eb49d22dd2385251
inthezonewithme <158035989+inthezonewithme@users.noreply.github.com>
2025-08-21 17:49:32 +0300

    Initialised empty repo

---
+++ backend/HackAMiliApp/src/main/java/com/hack_a_mili/app/HackAMiliApp/HackAMiliAppApplication.java
@@ -0,0 +1,13 @@
+package com.hack_a_mili.app.HackAMiliApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackAMiliAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackAMiliAppApplication.class, args);
+	}
+
+}
---
+++ backend/HackAMiliApp/src/test/java/com/hack_a_mili/app/HackAMiliApp/HackAMiliAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hack_a_mili.app.HackAMiliApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackAMiliAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
